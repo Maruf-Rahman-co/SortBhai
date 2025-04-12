7bb8bca0ce63ab5696e6a5eeb2e8e20b43a6fe8c
Maruf Rahman <mrmarufrahman.dev@gmail.com>
2025-04-12 19:35:59 +0400

    Some chnages that are related to Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   // Remove the fixed base path to make it work anywhere
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
   server: {
